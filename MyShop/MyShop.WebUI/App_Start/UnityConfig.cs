c0f71418f1a52153e44da61257e711a7bb0b6dea
Chase Sanders <msanders2657@gmail.com>
2020-05-23 17:37:15 -0400

    functional sql repository

--- MyShop/MyShop.WebUI/App_Start/UnityConfig.cs
+++ MyShop/MyShop.WebUI/App_Start/UnityConfig.cs
@@ -5,6 +5,7 @@ using System;
 using Unity;
 using MyShop.DataAccess.InMemory;
 using MyShop.Core.Models;
+using MyShop.DataAccess.SQL;
 
 namespace MyShop.WebUI
 {
@@ -46,8 +47,8 @@ namespace MyShop.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
     }
 }
